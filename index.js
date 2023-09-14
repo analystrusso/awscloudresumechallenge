11bfadb22b290c578eadc574803b0835874afdb0
analystrusso <adam.jordan.russo@gmail.com>
2023-09-13 21:53:31 -0400

    initialcommit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // JavaScript Code
 const counter = document.querySelector(".counter-number")
 async function updateCounter() {
-    let response = await fetch("https://zbbybk3lbtozfkvjacg6tfbfnq0bfzok.lambda-url.us-east-1.on.aws/");
+    let response = await fetch("https://jxdxvqu47ive4pvwl7nz7zpcfu0mbrth.lambda-url.us-east-1.on.aws/");
     let data = await response.json();
     counter.innerHTML = ` Views: ${data}`;
 }
